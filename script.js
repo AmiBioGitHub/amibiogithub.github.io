35d3bc2d38e6d201a90ea31524e1ef2364310331
AmiBioGitHub <154255499+AmiBioGitHub@users.noreply.github.com>
2025-09-15 10:47:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // === Configuration Webhooks - URLs DE PRODUCTION n8n ===
 
 // Sous-domaine configurable (changer ici seulement 👇)
-const N8N_SUBDOMAIN = "5uxf5dpzjr.eu.loclx.io";
+const N8N_SUBDOMAIN = "https://amibio.app.n8n.cloud";
 
 // Générateur d'URL webhook
 const WEBHOOK = (path) => `https://${N8N_SUBDOMAIN}/webhook/${path}`;
